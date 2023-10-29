fa9ac71070a84d2dfb4dc91683e260c19c145b2f
Angie Gao <jg4729@columbia.edu>
2023-10-29 15:50:52 -0400

    add graph team work

---
+++ Population by district (Bar).R
@@ -0,0 +1,17 @@
+# Load the necessary library
+library(ggplot2)
+
+# Read the CSV data
+data <- read.csv("lesotho_pop_short1.csv")
+
+# Create a bar chart to visualize the total population by district
+ggplot(data, aes(x = adm1_name, y = Total, fill = adm1_name)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Population by District in Lesotho",
+    x = "District",
+    y = "Total Population"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_discrete(name = "District")
