c00e24ab1fd15aeeba7ec8a00685b9e91e2c2dc1
Angie Gao <jg4729@columbia.edu>
2023-11-01 16:53:06 -0400

    Update Population by district bar graph

--- Population by district (Bar).R
+++ Population by district (Bar).R
@@ -7,6 +7,7 @@ data <- read.csv("lesotho_pop_short1.csv")
 # Create a bar chart to visualize the total population by district
 ggplot(data, aes(x = adm1_name, y = Total, fill = adm1_name)) +
   geom_bar(stat = "identity") +
+  geom_text(aes(label = Total), vjust = -0.5, size = 3) +  # Add population labels
   labs(
     title = "Population by District in Lesotho",
     x = "District",
@@ -14,4 +15,5 @@ ggplot(data, aes(x = adm1_name, y = Total, fill = adm1_name)) +
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_discrete(name = "District")
+  scale_fill_discrete(name = "District") +
+  guides(fill = FALSE)  # Remove the legend
